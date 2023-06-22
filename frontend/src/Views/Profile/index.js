506a0c54ffadca343ef278d4a97c923ca674d164
Jimmy <chjimmy661@gmail.com>
2023-06-22 17:49:24 +0800

    interest

---
+++ frontend/src/Views/Profile/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Profileform from "../../Components/ProfileForm";
+import NavBar from '../../Components/NavBar';
+
+export default function Login() {
+  return (
+    <div>
+      <NavBar/>
+      <Profileform ></Profileform>
+    </div>
+  )
+}
---
+++ frontend/src/Views/interest/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import InterestForm from "../../Components/InterestForm";
+import NavBar from '../../Components/NavBar';
+
+export default function Login() {
+  return (
+    <div>
+      <NavBar/>
+      <InterestForm></InterestForm>
+    </div>
+  )
+}
