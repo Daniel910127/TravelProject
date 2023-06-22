d84c6e8f20425cb2d66f41b200ced6d2e0d608dc
Jimmy <chjimmy661@gmail.com>
2023-06-22 17:50:30 +0800

    login_update

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,10 +4,12 @@ import Redirect from "../Components/Redirect";
 import Home from "../Views/Home";
 import Login from "../Views/Login";
 import NotFound from "../Views/NotFound";
+import Profile from "../Views/Profile";
 import Spot from "../Views/Spot";
 import Register from "../Views/Register";
+import Interest from "../Views/interest";
 import SearchSpot from "../Views/SearchSpot";
-import Itinerary from "../Views/Itinerary";
+import Itinerary from "../Components/Itinerary";
 export default function TravelRouter() {
   return (
     <Routes>
@@ -18,12 +20,12 @@ export default function TravelRouter() {
       {/* <Route index element={<Redirect to="/film/nowplaying" />} />  */}
       {/* <Route path="/film/nowplaying" element={<NowPlaying />} />
         <Route path="comingsoon" element={<Comingsoon />} /> */}
-      
+      <Route path="/itinerary" element={<Itinerary />} />
       <Route path="/spot/:s_Name" element={<Spot />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/itinerary" element={<Itinerary />} />
-
+      <Route path="/profile" element={<Profile/>} />
+      <Route path="/interest" element={<Interest />} />
       {/* <Route path="*" element={<Navigate to="/film"/>} /> */}
       <Route path="/" element={<Redirect to="/home" />} />
       <Route path="*" element={<NotFound />} />
