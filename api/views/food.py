879c95327ae9d6b84e51a18c43cc672e9c7b1423
Daniel <s0961102@gm.ncue.edu.tw>
2023-06-29 22:02:03 +0800

    景點foodhotel+jwt

--- api/views/food.py
+++ api/views/food.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse,HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated
 from ..serializers import AccountSerializer,SpotSerializer,MemberSerializer,s_InterestSerializer,FoodSerializer,Travel_ListSerializer,Travel_List_DetailSerializer,QuestionSerializer,s_PictureSerializer,m_PictureSerializer,foodWithPictureURLSerializer
 
 from ..models import Account,Spot,Member,s_Interest,Food,Travel_List,Travel_List_Detail,Question,s_Picture,m_Picture
@@ -23,6 +24,7 @@ def foodWithPictureList(request):
 	return Response(serializer.data)
 
 class FoodDetailView(generics.RetrieveAPIView):
+    permission_classes = (IsAuthenticated,)   
     serializer_class = foodWithPictureURLSerializer
 
     def get(self, request, f_Id):
--- api/views/hotel.py
+++ api/views/hotel.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse,HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated
 from ..serializers import AccountSerializer,SpotSerializer,MemberSerializer,s_InterestSerializer,FoodSerializer,HotelSerializer,Travel_ListSerializer,Travel_List_DetailSerializer,QuestionSerializer,s_PictureSerializer,m_PictureSerializer,foodWithPictureURLSerializer,hotelWithPictureURLSerializer
 
 from ..models import Account,Spot,Member,s_Interest,Food,Travel_List,Travel_List_Detail,Question,s_Picture,m_Picture,Hotel,h_Picture
@@ -23,6 +24,7 @@ def hotelWithPictureList(request):
 	return Response(serializer.data)
 
 class HotelDetailView(generics.RetrieveAPIView):
+    permission_classes = (IsAuthenticated,)    
     serializer_class = hotelWithPictureURLSerializer
 
     def get(self, request, h_Id):
